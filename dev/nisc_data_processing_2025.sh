2f75630fc61ece2bb0bcd1d7b54f75a2fc3a16ff
Bin-Guan <binguan2@gmail.com>
2025-10-28 14:28:45 -0400

    add old ref to make_bam_metadata,nisc_data_processing,mutserve version

--- dev/nisc_data_processing_2025.sh
+++ dev/nisc_data_processing_2025.sh
@@ -6,16 +6,25 @@
 
 #Create a project folder, then globus transfer data to the project folder such as eyeGENE_ORF15
 #run outside the project folder
-ProjectDir=$1
+ProjectDir=${1%/}
 TIMESTAMP=$(date "+%Y%m%d-%H%M%S")
-mkdir -p $ProjectDir-$TIMESTAMP/old_bam $ProjectDir-$TIMESTAMP/vcf_nisc/gvcf $ProjectDir-$TIMESTAMP/vcf_nisc/vcf
+mkdir -p $ProjectDir-$TIMESTAMP/old_bam $ProjectDir-$TIMESTAMP/vcf_nisc/gvcf $ProjectDir-$TIMESTAMP/vcf_nisc/vcf $ProjectDir-$TIMESTAMP/summary
 
-module load samtools
+module load samtools parallel
 
-find $ProjectDir -name "*.bam*" -exec mv {} $ProjectDir-$TIMESTAMP/old_bam \;
-find $ProjectDir -name "*.g.vcf.gz*" -exec mv {} $ProjectDir-$TIMESTAMP/vcf_nisc/gvcf \;
-find $ProjectDir -name "*.vcf*" -exec mv {} $ProjectDir-$TIMESTAMP/vcf_nisc/vcf \;
-for vcf in $ProjectDir-$TIMESTAMP/vcf_nisc/vcf/*.vcf; do bgzip -@ 8 $vcf; tabix -@ 8 $vcf.gz; done
+find $ProjectDir -type f -name "*.bam*" -exec mv {} $ProjectDir-$TIMESTAMP/old_bam \;
+find $ProjectDir -type f -name "*.g.vcf.gz*" -exec mv {} $ProjectDir-$TIMESTAMP/vcf_nisc/gvcf \;
+find $ProjectDir -type f -name "*.vcf*" -exec mv {} $ProjectDir-$TIMESTAMP/vcf_nisc/vcf \;
+find $ProjectDir -type f -name "*.txt" -exec mv {} $ProjectDir-$TIMESTAMP/summary \; 
+find $ProjectDir -type f -name "*.xls" -exec mv {} $ProjectDir-$TIMESTAMP/summary \;
+
+#for vcf in $ProjectDir-$TIMESTAMP/vcf_nisc/vcf/*.vcf; do bgzip -@ 8 $vcf; tabix -@ 8 $vcf.gz; done
+find "$ProjectDir-$TIMESTAMP/vcf_nisc/vcf" -type f -name '*.vcf' -print0 \
+| parallel -0 --no-run-if-empty -j 8 '
+    bgzip -@1 "{}" &&
+    tabix -@1 -p vcf "{}.gz"
+'
+
 mv $ProjectDir/*.* $ProjectDir-$TIMESTAMP/
 rm -rf $ProjectDir
 mv $ProjectDir-$TIMESTAMP $ProjectDir
